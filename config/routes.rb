15a4f37e33ee65ba87cfc9deecb6b413ec5a5b27
TheNinJedi <TheNinJedi@gmail.com>
2013-11-18 23:48:11 -0700

    Creates Home Controller. Adds database.yml file to gitignore.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 HomeTeachingApp::Application.routes.draw do
+  get "home/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 root :to => "home#index"
