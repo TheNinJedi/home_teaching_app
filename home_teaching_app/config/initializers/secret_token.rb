ec230b2777bcc4049eb4801cd9d8d28a95bb08c8
TheNinJedi <TheNinJedi@gmail.com>
2013-11-18 21:44:26 -0700

    Initial Commit

---
+++ home_teaching_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HomeTeachingApp::Application.config.secret_token = '6e4fd0b3077363d4c04e920cb2c2397acbd6ca8944bc2cf311ae094cde90ffefded1cac10094ef81e5b6e058f96e24bfddcf0792625932bae6bfcd4856dc9690'
